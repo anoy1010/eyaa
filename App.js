e1f78c1b11c709967c9c415c2c68483c3086f0ca
anoy1010 <jojoletag@gmail.com>
2024-06-08 13:44:04 +0200

    version build

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import Routes from "./routes";
+import { SplashScreen } from 'expo-splash-screen';
 import {
   useFonts,
   Poppins_100Thin,
@@ -22,7 +23,6 @@ import {
   Poppins_900Black,
   Poppins_900Black_Italic,
 } from "@expo-google-fonts/poppins";
-import AppLoading from "expo-app-loading";
 
 const App = () => {
   let [fontsLoaded] = useFonts({
@@ -45,15 +45,54 @@ const App = () => {
     Poppins_900Black,
     Poppins_900Black_Italic,
   });
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <View style={{ flex: 1 }}>
-        <Routes />
-      </View>
-    );
-  }
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Preload assets, then show the app screen
+        await Font.loadAsync({
+          Poppins_100Thin,
+          Poppins_100Thin_Italic,
+          Poppins_200ExtraLight,
+          Poppins_200ExtraLight_Italic,
+          Poppins_300Light,
+          Poppins_300Light_Italic,
+          Poppins_400Regular,
+          Poppins_400Regular_Italic,
+          Poppins_500Medium,
+          Poppins_500Medium_Italic,
+          Poppins_600SemiBold,
+          Poppins_600SemiBold_Italic,
+          Poppins_700Bold,
+          Poppins_700Bold_Italic,
+          Poppins_800ExtraBold,
+          Poppins_800ExtraBold_Italic,
+          Poppins_900Black,
+          Poppins_900Black_Italic,
+        });
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        // Handle loading error
+        console.warn(e);
+      }
+    }
+  
+    prepare();
+  }, []);
+  
+  const onLayoutRootView = useCallback(async () => {
+    if (SplashScreen.isVisible()) {
+      // This tells the splash screen to hide immediately! If there's a delay between hiding
+      // the splash screen and showing the first frame, then the user will see a blank screen
+      // while the app is loading its initial state.
+      await SplashScreen.hideAsync();
+    }
+  }, []);
+  
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Routes />
+    </View>
+  );
 }
 
 export default App;
